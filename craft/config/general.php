e9f022771cce32db8e51ce6524f2709799f0da37
David Rhoden <david@davidrhoden.com>
2019-08-16 02:14:13 -0500

    Update general.php

--- craft/config/general.php
+++ craft/config/general.php
@@ -7,33 +7,6 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-// return array(
-
-// 	// Base site URL
-// 	'siteUrl' => null,
-
-// 	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-// 	'generateTransformsBeforePageLoad' => true,
-// 	'environmentVariables' => array(),
-
-// 	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-// 	'defaultWeekStartDay' => 0,
-
-// 	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-// 	'enableCsrfProtection' => true,
-
-// 	// Whether "index.php" should be visible in URLs (true, false, "auto")
-// 	'omitScriptNameInUrls' => 'auto',
-
-// 	// Control Panel trigger word
-// 	'cpTrigger' => 'admin',
-
-// 	// Dev Mode (see https://craftcms.com/support/dev-mode)
-// 	'devMode' => true,
-
-// );
-
-
 return array(
   'lukespurrallen.local' => array(
     'devMode' => true,
@@ -48,13 +21,13 @@ return array(
   ),
 
   // Use IP address of your droplet below
-  'lukespurrallen.com' => array(
-    'siteUrl' => 'http://lukespurrallen.com/',
-    'environmentVariables' => array(
-      'basePath' => '../public/',
-      'baseUrl'  => 'http://lukespurrallen.com/',
-    ),
-    'omitScriptNameInUrls' => true,
+    'lukespurrallen.com' => array(
+      'environmentVariables' => array(
+          'basePath' => '/home/forge/lukespurrallen.com/public/',
+          'baseUrl'  => '//lukespurrallen.com/',
+      ),
+      'omitScriptNameInUrls' => true,
+      'devMode' => true,
   ),
     'lukespurrallen.xyz' => array(
       'environmentVariables' => array(
